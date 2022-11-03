1d0a6978c1c731250abe95517507002688ef3ff2
Edmar <jose.fortyfive@gmail.com>
2022-11-03 16:19:46 +0800

    Initial commit

---
+++ untitled/src/input/KeyHandler.java
@@ -0,0 +1,84 @@
+package input;
+
+import main.GamePanel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    GamePanel gp;
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    public boolean checkedDrawTime = false;
+
+
+    public KeyHandler(GamePanel gp){
+        this.gp = gp;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            upPressed = true;
+        }
+
+        if(code == KeyEvent.VK_S){
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_D){
+            rightPressed = true;
+        }
+        if(code == KeyEvent.VK_P){
+            if(gp.gameState == gp.playState){
+                gp.gameState = gp.pauseState;
+            }
+            else if(gp.gameState == gp.pauseState){
+                gp.gameState = gp.playState;
+            }
+        }
+
+
+        //DEBUG
+        if(code == KeyEvent.VK_T){
+            if(checkedDrawTime == false){
+                checkedDrawTime = true;
+            }
+            else if(checkedDrawTime == true){
+                checkedDrawTime = false;
+
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W){
+            upPressed = false;
+        }
+
+        if(code == KeyEvent.VK_S){
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_D){
+            rightPressed = false;
+        }
+    }
+}
